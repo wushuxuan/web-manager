c3a22c9ad677705c187f06ffed227185e3f136c5
吴姝璇 <wshuxuan@extremevalue.cn>
2019-12-31 10:09:51 +0800

    更新

--- _mock/_user.ts
+++ _mock/_user.ts
@@ -100,8 +100,8 @@ export const USERS = {
   'POST /user/avatar': 'ok',
   'POST /login/account': (req: MockRequest) => {
     const data = req.body;
-    if (!(data.userName === 'admin' || data.userName === 'user') || data.password !== 'ng-alain.com') {
-      return { msg: `Invalid username or password（admin/ng-alain.com）` };
+    if (!(data.userName === 'admin' || data.userName === 'user') || data.password !== '123456') {
+      return { msg: `Invalid username or password（admin/123456）` };
     }
     return {
       msg: 'ok',
